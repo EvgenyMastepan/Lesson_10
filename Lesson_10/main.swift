5c6388d2437421b9aca0ba3f38872b3b6f29ad37
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-10-19 11:45:26 +0700

    Lesson_10 is begin.

---
+++ Lesson_10/main.swift
@@ -0,0 +1,14 @@
+//
+//  swift. 9th stream
+//  Lesson_10
+//
+//  Created by Evgeny Mastepan on 19.10.2024.
+//
+
+import Foundation
+
+/* Задача 1: Создайте протокол Playable: Метод: play(), который не принимает параметров и ничего не возвращает. */
+
+protocol Playable {
+    func play()
+}
